7ef44d538c9878e28a3d47cc97ad1ffd0a8ded08
asherhoffman1120 <asher.hoffman1120@gmail.com>
2022-06-17 14:16:05 -0400

    drop the hackathon part of the package name

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,5 @@
 // This is a script for deploying your contracts. You can adapt it to deploy
 // yours, or create new ones.
-
-const path = require("path");
-
 async function main() {
   // This is just a convenience check
   if (network.name === "hardhat") {
@@ -34,21 +31,21 @@ async function main() {
 
 function saveFrontendFiles(token) {
   const fs = require("fs");
-  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+  const contractsDir = __dirname + "/../frontend/src/contracts";
 
   if (!fs.existsSync(contractsDir)) {
     fs.mkdirSync(contractsDir);
   }
 
   fs.writeFileSync(
-    path.join(contractsDir, "contract-address.json"),
+    contractsDir + "/contract-address.json",
     JSON.stringify({ Token: token.address }, undefined, 2)
   );
 
   const TokenArtifact = artifacts.readArtifactSync("Token");
 
   fs.writeFileSync(
-    path.join(contractsDir, "Token.json"),
+    contractsDir + "/Token.json",
     JSON.stringify(TokenArtifact, null, 2)
   );
 }
